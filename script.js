3c9022d4c4b74e827c27107050ee0b71c36b260e
oDevSilva <ocontato.dev@gmail.com>
2023-05-19 11:57:42 -0300

    Adicionado o título a página

---
+++ script.js
@@ -0,0 +1,8 @@
+const titleELement = document.createElement("h1");
+titleELement.setAttribute("id", "title");
+titleELement.textContent = "Paleta de Cores";
+document.body.appendChild(titleELement);
+
+/*-------------------------------*/
+
+
